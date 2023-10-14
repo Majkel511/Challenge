58fea2be4799ba76fc9ea1c5c7ee5aea6cf983ce
Magda <Magda@LAPTOP-EB64A29G>
2023-10-14 15:41:42 +0100

    Poprawienie pracy domowej

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,18 +4,28 @@ var old = 33;
 
 if (name == "Michal" && old == 33 && gender == "Men")
 {
-    Console.WriteLine("Jestes mężczyzna o imieniu Michal i masz 33 lata");
+    Console.WriteLine("Michal, lat 33");
 }
-    else if (name == "Michal" && old >= 34)
-    {
-    Console.WriteLine("Michal masz wiecej lat niz 34");
-    }
-
-    else if (name == "Michal" && old <= 32)
-    {
-    Console.WriteLine("Michal masz mniej lat niz 32");
-    }
-
+else if (old >= 34 && gender == "Men")
+{
+    Console.WriteLine("Meżczyzna starszy od 34 lat");
+}
+else if (old <= 32 && gender == "Men")
+{
+    Console.WriteLine("Meżczyzna młodszy od 32 lat");
+}
+else if (old < 32 && gender == "Woman")
+{
+    Console.WriteLine("Kobieta młodsza od 32 lat");
+}
+else if (old == 32 && gender == "Woman")
+{
+    Console.WriteLine("Kobieta w wieku 32 lat");
+}
+else if (old > 32 && gender == "Woman")
+{
+    Console.WriteLine("Kobieta starsza od 32 lat");
+}
 else
 {
     Console.WriteLine("Warunek niespelniony ");
