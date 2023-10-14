b446229a699f82d25494262f9f062ea1886afe53
Magda <Magda@LAPTOP-EB64A29G>
2023-10-14 14:58:06 +0100

    Zadanie Domowe Z Dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Michal";
+var gender = "Men";
+var old = 33;
+
+if (name == "Michal" && old == 33 && gender == "Men")
+{
+    Console.WriteLine("Jestes mężczyzna o imieniu Michal i masz 33 lata");
+}
+    else if (name == "Michal" && old >= 34)
+    {
+    Console.WriteLine("Michal masz wiecej lat niz 34");
+    }
+
+    else if (name == "Michal" && old <= 34)
+    {
+    Console.WriteLine("Michal masz mniej lat niz 32");
+    }
+
+else
+{
+    Console.WriteLine("Warunek niespelniony ");
+}
