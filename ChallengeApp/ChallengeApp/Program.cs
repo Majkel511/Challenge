bde0dd7da930d4a0604edf4270e5b7a8849e71ac
Magda <Magda@LAPTOP-EB64A29G>
2023-10-17 18:21:32 +0100

    Praca Domowa z Dnia 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,87 +1,45 @@
-﻿// Przygotuj program, ktory policzy ile jakich cyfr
-// występuje w podanej liczbie:
+﻿using ChallengeApp;
 
-// Przydkład:
-// Wynik dla liczby: 4566
-// 0 => 0
-// 1 => 0
-// 2 => 0
-// 3 => 0
-// 4 => 1
-// 5 => 1
-// 6 => 2
-// 7 => 0
-// 8 => 0
-// 9 => 0
+Employee user1 = new Employee("Adam", "Mickiewicz", 57);
+Employee user2 = new Employee("Fryderyk", "Chopin", 39);
+Employee user3 = new Employee("ludwig", "Beethoven", 57);
 
-
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-
-var container0 = 0;
-var container1 = 0;
-var container2 = 0;
-var container3 = 0;
-var container4 = 0;
-var container5 = 0;
-var container6 = 0;
-var container7 = 0;
-var container8 = 0;
-var container9 = 0;
-
-foreach (char letter in letters)
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == 0)
-    {
-        container0++;
-    }
-    else if (letter == '1')
-    {
-        container1++;
-    }
-    else if (letter == '2')
-    {
-        container2++;
-    }
-    else if (letter == '3')
-    {
-        container3++;
-    }
-    else if (letter == '4')
-    {
-        container4++;
-    }
-    else if (letter == '5')
-    {
-        container5++;
-    }
-    else if (letter == '6')
-    {
-        container6++;
-    }
-    else if (letter == '7')
-    {
-        container7++;
-    }
-    else if (letter == '8')
-    {
-        container8++;
-    }
-    else if (letter == '9')
-    {
-        container9++;
-    }
+    user1, user2, user3
+};
 
+user1.AddGrade(5);
+user1.AddGrade(2);
+user1.AddGrade(8);
+user1.AddGrade(1);
+user1.AddGrade(9);
+user1.AddGrade(0);
+
+user2.AddGrade(8);
+user2.AddGrade(9);
+user2.AddGrade(9);
+user2.AddGrade(2);
+user2.AddGrade(5);
+
+user3.AddGrade(3);
+user3.AddGrade(4);
+user3.AddGrade(5);
+user3.AddGrade(6);
+user3.AddGrade(7);
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+foreach (Employee employee in employees)
+{
+    if (employee.Score > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employee.Score;
+    }
 }
-Console.WriteLine("Twoja liczba do rozbicia to " + number + ".");
-Console.WriteLine("Ile Pojawia sie cyfrt 0:" + container0);
-Console.WriteLine("Ile Pojawia sie cyfrt 1:" + container1);
-Console.WriteLine("Ile Pojawia sie cyfrt 2:" + container2);
-Console.WriteLine("Ile Pojawia sie cyfrt 3:" + container3);
-Console.WriteLine("Ile Pojawia sie cyfrt 4:" + container4);
-Console.WriteLine("Ile Pojawia sie cyfrt 5:" + container5);
-Console.WriteLine("Ile Pojawia sie cyfrt 6:" + container6);
-Console.WriteLine("Ile Pojawia sie cyfrt 7:" + container7);
-Console.WriteLine("Ile Pojawia sie cyfrt 8:" + container8);
-Console.WriteLine("Ile Pojawia sie cyfrt 9:" + container9);
+Console.WriteLine("Najlepszy pracownik pod katem punktowym to:");
+Console.WriteLine(employeeWithMaxResult.Name);
+Console.WriteLine($"{employeeWithMaxResult.Surname}: ");
+Console.WriteLine($"{employeeWithMaxResult.Score}: pts.");
+Console.WriteLine("Brawo byle by tak dalej.:");
